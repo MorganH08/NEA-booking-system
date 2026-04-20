abd853ddb1749b382ddf10564adfd06ca9af27ee
MorganH08 <hopkin.mg@oundleschool.org.uk>
2026-04-20 09:59:37 +0100

    jhf

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="password";
+$dbname="nea_booking_system";
+
+try{
+    $conn= new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo("Connected successfully<br>"); #message displayed if connection is successful
+}
+catch(PDOException $e){
+    echo("Connection failed: " . $e->getMessage()); #error message if connection doesnt work
+}
+?>
